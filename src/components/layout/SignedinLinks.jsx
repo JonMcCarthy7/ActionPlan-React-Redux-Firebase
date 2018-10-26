fe9f51cdfe5a3d4d601fce7a795b1d0149026de2
Jon McCarthy <jtmcc93@gmail.com>
2018-10-25 18:23:27 -0700

    add navbar signin/signout links

---
+++ src/components/layout/SignedinLinks.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const SignedInLinks = props => {
+  return (
+    <ul className="right">
+      <li>
+        <NavLink to="/">New Project</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Log Out</NavLink>
+      </li>
+      <li>
+        {" "}
+        <NavLink to="/" className="btn btn-floating red darken-1">
+          a
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default SignedInLinks;
