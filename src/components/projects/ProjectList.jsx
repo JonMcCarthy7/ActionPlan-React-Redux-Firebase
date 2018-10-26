47e2f2222ecf318c61cf1a1cb8b21077e3f88c16
Jon McCarthy <jtmcc93@gmail.com>
2018-10-26 08:51:05 -0700

    create project Summary componenet

--- src/components/projects/ProjectList.jsx
+++ src/components/projects/ProjectList.jsx
@@ -1,36 +1,14 @@
 import React from "react";
+import ProjectSummary from "./ProjectSummary";
 
 const ProjectList = () => {
   return (
-    <React.Fragment>
-      <div className="project-list section">
-        <div className="card z-depth-0 project-summary">
-          <div className="card-content grey-text text-darken-3">
-            <span className="card-title">Project Title here</span>
-            <p>Posted by: </p>
-            <p className="grey-text">Date</p>
-          </div>
-        </div>
-      </div>
-      <div className="project-list section">
-        <div className="card z-depth-0 project-summary">
-          <div className="card-content grey-text text-darken-3">
-            <span className="card-title">Project Title here</span>
-            <p>Posted by: </p>
-            <p className="grey-text">Date</p>
-          </div>
-        </div>
-      </div>
-      <div className="project-list section">
-        <div className="card z-depth-0 project-summary">
-          <div className="card-content grey-text text-darken-3">
-            <span className="card-title">Project Title here</span>
-            <p>Posted by: </p>
-            <p className="grey-text">Date</p>
-          </div>
-        </div>
-      </div>
-    </React.Fragment>
+    <div className="project-list section">
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
+    </div>
   );
 };
 
