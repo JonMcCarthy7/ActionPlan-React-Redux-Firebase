f80344a12155f78921785c09d019a37b091544f1
Jon McCarthy <jtmcc93@gmail.com>
2018-10-26 08:45:23 -0700

    add dashboard, projectlist, and notification components along with dashboard route

---
+++ src/components/projects/ProjectList.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const ProjectList = () => {
+  return (
+    <React.Fragment>
+      <div className="project-list section">
+        <div className="card z-depth-0 project-summary">
+          <div className="card-content grey-text text-darken-3">
+            <span className="card-title">Project Title here</span>
+            <p>Posted by: </p>
+            <p className="grey-text">Date</p>
+          </div>
+        </div>
+      </div>
+      <div className="project-list section">
+        <div className="card z-depth-0 project-summary">
+          <div className="card-content grey-text text-darken-3">
+            <span className="card-title">Project Title here</span>
+            <p>Posted by: </p>
+            <p className="grey-text">Date</p>
+          </div>
+        </div>
+      </div>
+      <div className="project-list section">
+        <div className="card z-depth-0 project-summary">
+          <div className="card-content grey-text text-darken-3">
+            <span className="card-title">Project Title here</span>
+            <p>Posted by: </p>
+            <p className="grey-text">Date</p>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default ProjectList;
